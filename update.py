836b346451fb21a222d56d4dce57b317904a4dff
Maxime LAPLANCHE <laplanche.maxime@outlook.fr>
2023-02-22 16:57:32 +0100

    🔐Verify ssl certificate on images update [skip ci]

--- update.py
+++ update.py
@@ -71,7 +71,7 @@ php_version_by_dolibarr_version = {
 }
 
 # Fetch the tags for Dolibarr from GitHub API
-dolibarr_tags = requests.get("https://api.github.com/repos/dolibarr/dolibarr/tags", verify=False)
+dolibarr_tags = requests.get("https://api.github.com/repos/dolibarr/dolibarr/tags", verify=True)
 dolibarr_tags_obj = json.loads(dolibarr_tags.text)
 
 # Loop through the tags to find the versions of Dolibarr to build images for
